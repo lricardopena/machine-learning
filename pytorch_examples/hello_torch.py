a046c78bf4111719f30d985b9ada9281d13eadbe
Luis Pena <luis.pena@trackstreet.com>
2021-04-07 23:23:16 -0500

    Add more functionalities to the hello word for pytorch

--- pytorch_examples/hello_torch.py
+++ pytorch_examples/hello_torch.py
@@ -3,3 +3,20 @@ x = torch.rand(5, 3)
 print(x)
 
 print(f'Is cuda available in the system: {torch.cuda.is_available()}')
+
+# Check that this time the variable is in the CPU
+print(x.device)
+
+# Now we change it to CUDA
+x_cuda = x.to("cuda")
+print(x_cuda.device)
+
+# Show the size of the tensor
+
+print(f'x.shape {x.shape}')
+print(f'x.size {x.size()}')
+
+# Convert the tensor to numpy array:
+x_numpy = x_cuda.cpu().detach().numpy()
+
+print(f'Numpy array: \n {x_numpy}')
